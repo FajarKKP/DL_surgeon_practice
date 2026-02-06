c2854db15ffaaf17015b4a257ae013d69d9e9b72
fajar_kkp <fajarputra4231@gmail.com>
2026-02-06 20:09:28 +0700

    deliberate resblock testing

--- residual_block.py
+++ residual_block.py
@@ -8,9 +8,9 @@ x = torch.randn(1, 3, 32, 32)
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, padding= 1)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, stride = 2, padding= 1)
         self.conv2 = nn.Conv2d(out_channels, out_channels, 3, padding= 1)
-        self.skip = nn.Conv2d(in_channels, out_channels, 1) if in_channels != out_channels else nn.Identity()
+        self.skip = nn.Conv2d(in_channels, out_channels, 1, stride = 2) if in_channels != out_channels else nn.Identity()
 
     def forward(self, x):
         skip = self.skip(x)
