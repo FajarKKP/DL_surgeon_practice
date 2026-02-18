335cc613590cf98a825b4db9283645843a27289f
fajar_kkp <fajarputra4231@gmail.com>
2026-02-18 14:00:48 +0700

    channel impact test

--- cnn.py
+++ cnn.py
@@ -23,14 +23,14 @@ transform_test = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_test)
-trainloader = torch.utils.data.DataLoader(trainset,batch_size=batch_size_initial, shuffle = True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset,batch_size=batch_size_initial, shuffle = True, num_workers= 0)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download = True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size= batch_size_initial, shuffle = False, num_workers= 2)
+testloader = torch.utils.data.DataLoader(testset, batch_size= batch_size_initial, shuffle = False, num_workers= 0)
 
 # Model
 class Cnn(nn.Module):
-    def __init__(self, c1,c2,c3):
+    def __init__(self, c1,c2,c3,c4,c5,c6):
         super(Cnn, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3,c1, kernel_size=3, padding=1),
@@ -40,10 +40,17 @@ class Cnn(nn.Module):
             nn.MaxPool2d(2),
             nn.Conv2d(c2,c3, kernel_size=3, padding=1),
             nn.ReLU(),
+            nn.Conv2d(c3,c4, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(c4, c5, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(c5,c6, kernel_size=3, padding=1),
+            nn.ReLU(),
             nn.AdaptiveAvgPool2d(1)
         )
 
-        self.classifier = nn.Linear(c3, 10)
+        self.classifier = nn.Linear(c6, 10)
 
     def forward(self, x):
         x = self.features(x)
@@ -120,19 +127,20 @@ def evaluate_model(model, name):
 # ----------------------------
 # Run Baseline
 # ----------------------------
-print("=== BASELINE MODEL ===")
-baseline = Cnn(32, 64, 128).to(device)
-train_model(baseline)
-evaluate_model(baseline, "Baseline")
+if __name__ == "__main__":
+    print("=== BASELINE MODEL ===")
+    baseline = Cnn(32, 64, 64, 128, 128, 256).to(device)
+    train_model(baseline)
+    evaluate_model(baseline, "Baseline")
 
-# ----------------------------
-# Run Double Width
-# ----------------------------
-print("\n=== DOUBLE WIDTH MODEL ===")
-double_width = Cnn(64, 128, 256).to(device)
-train_model(double_width)
-evaluate_model(double_width, "Double Width")
+    # print("\n=== DOUBLE WIDTH MODEL ===")
+    # double_width = Cnn(64, 128, 256).to(device)
+    # train_model(double_width)
+    # evaluate_model(double_width, "Double Width")
 
-
+    print("=== ANOTHER MODEL ===")
+    another_baseline = Cnn(32, 32, 64, 64, 128, 256).to(device)
+    train_model(another_baseline)
+    evaluate_model(another_baseline, "Another Baseline")
 
 
